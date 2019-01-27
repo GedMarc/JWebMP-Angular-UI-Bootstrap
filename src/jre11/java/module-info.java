36ffa34ed14bcc439330e9e55596da651c2192cc
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:57 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,11 +1,3 @@
-import com.jwebmp.core.base.angular.services.IAngularModule;
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.angularuibootstrap.AngularUIBootstrapModule;
-import com.jwebmp.plugins.angularuibootstrap.AngularUIBootstrapPageConfigurator;
-import com.jwebmp.plugins.angularuibootstrap.implementations.AngularUIBootstrapExclusionsModule;
-
 module com.jwebmp.plugins.angularuibootstrap {
 	exports com.jwebmp.plugins.angularuibootstrap;
 
@@ -14,10 +6,10 @@ module com.jwebmp.plugins.angularuibootstrap {
 	requires com.jwebmp.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
-	provides IPageConfigurator with AngularUIBootstrapPageConfigurator;
-	provides IAngularModule with AngularUIBootstrapModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularuibootstrap.AngularUIBootstrapPageConfigurator;
+	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.angularuibootstrap.AngularUIBootstrapModule;
 
-	provides IGuiceScanModuleExclusions with AngularUIBootstrapExclusionsModule;
-	provides IGuiceScanJarExclusions with AngularUIBootstrapExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularuibootstrap.implementations.AngularUIBootstrapExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularuibootstrap.implementations.AngularUIBootstrapExclusionsModule;
 
 }
